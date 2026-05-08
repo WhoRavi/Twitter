2ca630447c4beb55ae6354cb9a8410449e9a93bf
Ravi Devgam <ravidevgam@gmail.com>
2026-05-08 11:01:09 +0530

    remove old project files, keep only X API pipeline

--- run_main.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Batch script to run main.py with proper Python environment
-REM Created for Windows Task Scheduler
-
-cd /d "c:\Users\ravid\Desktop\Codes\Projects\Twitter"
-
-REM Run the Python script
-python main.py
